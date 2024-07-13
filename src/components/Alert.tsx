a6796f93f371120812d7cc93243b63057da13798
cliff-4 <adityamishra0700@gmail.com>
2024-07-14 01:39:36 +0530

    feat: alert type changes on click

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,11 +1,34 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 interface AlretProp {
   children: ReactNode;
 }
 
+let alertTypes = [
+  "light",
+  "dark",
+  "primary",
+  "secondary",
+  "success",
+  "danger",
+  "warning",
+  "info",
+];
+let currAlertType = 0;
+
 const Alert = ({ children }: AlretProp) => {
-  return <div className="alert alert-info">{children}</div>;
+  const [alertType, setAlertType] = useState(alertTypes[currAlertType]);
+
+  const alertUpdateHandler = () => {
+    currAlertType = (currAlertType + 1) % alertTypes.length;
+    setAlertType(alertTypes[currAlertType]);
+  };
+
+  return (
+    <div className={`alert alert-${alertType}`} onClick={alertUpdateHandler}>
+      {children}
+    </div>
+  );
 };
 
 export default Alert;
