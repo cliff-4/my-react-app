c6b31545b4ab5080261cde5bfbdb31ac468a6404
cliff-4 <adityamishra0700@gmail.com>
2024-07-14 03:26:39 +0530

    misc: updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import ListGroup from "./components/ListGroup";
-import Alert from "./components/Alert";
-import ChangingButton from "./components/ChangingButton";
+import AlertButton from "./components/AlertButton";
 
 function App() {
-  let alertprops = {
-    content: (
-      <>
-        Amber alert <h1>Serious</h1>
-      </>
-    ),
-  };
-
-  let cities = {
-    items: ["Mumbai", "New York", "London", "Paris"],
-    heading: "Cities",
-  };
-
   return (
     <>
-      <ListGroup items={cities.items} heading={cities.heading} />
-      <Alert>{alertprops.content}</Alert>
-      <ChangingButton />
+      <AlertButton>Obi wan kenobi!</AlertButton>
     </>
   );
 }
