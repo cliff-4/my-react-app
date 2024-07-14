62aba454a844c5d64dd815a73017432d325e92ad
cliff-4 <adityamishra0700@gmail.com>
2024-07-15 02:46:44 +0530

    feat: added colour palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#240115', // dark-purple
+        secondary: '#de3c4b', // rusty-red
+        tertiary: '#87f5fb', // electric-blue
+        quarternary: '#2f131e', // dark-purple-2
+        quinternary: '#cec3c1', // silver
+      }
+    },
   },
   plugins: [],
 }
