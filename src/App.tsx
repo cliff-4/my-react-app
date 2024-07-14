002a6c5fadf8070c92a0fb5129e78994d7a6a152
cliff-4 <adityamishra0700@gmail.com>
2024-07-15 02:48:20 +0530

    feat: dynamically changing page with sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+import Sidebar from "./components/sidebar";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const Pages = [
+    <div>Home page</div>,
+    <TodoList />,
+    <div>Page 3</div>,
+    <div>Page 4</div>,
+  ];
+
+  const [pageIndex, setPageIndex] = useState(0);
   return (
-    <>
-      <TodoList></TodoList>
-    </>
+    <div className="flex-row">
+      <Sidebar pageSetter={setPageIndex} />
+      {Pages[pageIndex]}
+    </div>
   );
 }
 
