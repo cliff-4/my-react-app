7015f16247a104b911c4fa226b93e1e787807019
cliff-4 <adityamishra0700@gmail.com>
2024-07-16 00:38:44 +0530

    feat: added cool animation

--- src/components/DefaultPage.tsx
+++ src/components/DefaultPage.tsx
@@ -1,17 +1,36 @@
+import { FaHouseChimneyWindow } from "react-icons/fa6";
+import FadeIn from "./FadeIn";
+
 interface PageProps {
   content: string;
 }
 
 const DefaultPage = ({ content }: PageProps) => {
   return (
-    <div className="bg-tertiary min-h-screen flex flex-row justify-center">
+    <div className="min-h-screen flex flex-row justify-center">
       <div className="min-w-[100vw] flex flex-col justify-center items-center">
-        <p className="flex flex-row justify-center text-7xl max-w-[40vw] bg-primary text-secondary m-4 p-8 rounded-full">
-          Default page
-        </p>
-        <p className="italic p-4 my-2 bg-primary text-tertiary rounded-full">
-          {content}
-        </p>
+        <FadeIn delay="0">
+          <p
+            className="
+            flex flex-row justify-center
+            text-7xl max-w-[40vw] m-4 p-8 rounded-2xl
+            bg-quarternary text-secondary
+            border-solid border-2 border-quinternary
+            "
+          >
+            Default page
+          </p>
+        </FadeIn>
+        <FadeIn delay="200">
+          <p className="italic p-4 my-2 bg-quarternary text-secondary rounded-2xl border-solid border-2 border-quinternary">
+            {content}
+          </p>
+        </FadeIn>
+        <FadeIn delay="400">
+          <div className="my-10 animate-bounce text-quinternary">
+            <FaHouseChimneyWindow size="64" />
+          </div>
+        </FadeIn>
       </div>
     </div>
   );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, KeyboardEvent } from "react";
 import { RxCrossCircled } from "react-icons/rx";
+import FadeIn from "./FadeIn";
 
 interface Todo {
   text: string;
@@ -20,7 +21,7 @@ const TodoList: React.FC = () => {
 
   const addTodo = (text: string) => {
     if (text === "") return;
-    setTodos([{ text, completed: false }, ...todos]);
+    setTodos([...todos, { text, completed: false }]);
   };
 
   const removeTodo = (index: number) => {
@@ -36,40 +37,51 @@ const TodoList: React.FC = () => {
 
   return (
     <div className="flex justify-center flex-row min-h-screen">
-      <div className="flex justify-start flex-col min-w-[50vh] max-w-[50vh]">
-        <div className="border-solid border-2 border-quinternary p-10 mt-[15vh] rounded-2xl bg-primary max-h-[70vh]">
-          <div className="border-solid border-2 border-quinternary p-5 rounded-2xl bg-secondary flex flex-col">
-            <p className="text-lg text-center font-extrabold font-mono">
-              To-do List
-            </p>
-            <input
-              type="text"
-              onKeyDown={handleKeyDown}
-              className="bg-quinternary max-w-md px-4 py-1 rounded-md mt-2"
-            />
-          </div>
+      <div className="flex flex-col justify-start min-w-[50vh] max-w-[50vh]">
+        <FadeIn className="border-solid border-2 border-quinternary p-10 mt-[15vh] rounded-2xl bg-quarternary max-h-[70vh]">
+          <FadeIn
+            delay="200"
+            className="border-solid border-2 border-quinternary p-5 rounded-2xl bg-secondary flex flex-col"
+          >
+            <FadeIn delay="400">
+              <p className="text-lg text-center font-extrabold font-mono">
+                To-do List
+              </p>
+            </FadeIn>
+            <FadeIn delay="600">
+              <div className="flex flex-col">
+                <input
+                  type="text"
+                  onKeyDown={handleKeyDown}
+                  className="bg-quinternary max-w-md px-4 py-1 rounded-md mt-2"
+                />
+              </div>
+            </FadeIn>
+          </FadeIn>
           <div>
             <ul>
               {todos.map((todo, index) => (
-                <li
-                  key={index}
-                  className="border-solid border-2 border-quinternary my-2 py-1 pl-4 pr-1 h-auto rounded-2xl
-                  flex flex-row justify-between font-bold font-mono text-pretty
-                  max-w-full
-                  bg-[#f76371]
-                  "
-                >
-                  <p className="text-ellipsis overflow-hidden max-w-64">
-                    {todo.text}
-                  </p>
-                  <button onClick={() => removeTodo(index)}>
-                    <RxCrossCircled size={20} />
-                  </button>
-                </li>
+                <FadeIn>
+                  <li
+                    key={index}
+                    className="border-solid border-2 border-quinternary my-2 py-1 pl-4 pr-1 h-auto rounded-2xl
+                    flex flex-row justify-between font-bold font-mono text-pretty
+                    max-w-full
+                    bg-[#f76371]
+                    "
+                  >
+                    <p className="text-ellipsis overflow-hidden max-w-64">
+                      {todo.text}
+                    </p>
+                    <button onClick={() => removeTodo(index)}>
+                      <RxCrossCircled size={20} />
+                    </button>
+                  </li>
+                </FadeIn>
               ))}
             </ul>
           </div>
-        </div>
+        </FadeIn>
       </div>
     </div>
   );
