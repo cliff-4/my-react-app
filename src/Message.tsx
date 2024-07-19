cde0b3069c9c58c28cc6568928bf19ddf7cece54
cliff-4 <adityamishra0700@gmail.com>
2024-07-20 01:05:45 +0530

    misc: clean-up

--- src/Message.tsx
+++
@@ -1,8 +0,0 @@
-// Using PascalCase for all the function components
-
-function Message() {
-    // This is JSX (JavaScript XML)
-    return <h2>I am motivated :)</h2>;
-}
-
-export default Message;
