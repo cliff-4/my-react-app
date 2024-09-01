1b260bd3643b5c1f3a4eae5e11f1ea2893329ace
cliff-4 <adityamishra0700@gmail.com>
2024-09-02 05:08:13 +0530

    misc: moved projects to .tsx

---
+++ src/assets/ProjectsList.tsx
@@ -0,0 +1,12 @@
+export const ProjectsList = [
+  {
+    title: "My portfolio website",
+    description: "It's this very website you're seeing!",
+    link: "https://github.com/cliff-4/my-react-app",
+  },
+  {
+    title: "Project 2",
+    description: "This is a project 2 description",
+    link: "#",
+  },
+];
